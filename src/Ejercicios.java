9b0d8c52de2c6df77c6c4daea32dc11df7d95184
Alumno <alumno@localhost.localdomain>
2014-06-16 08:50:37 -0600

    ExamenProgramacion

---
+++ src/Ejercicios.java
@@ -0,0 +1,68 @@
+
+
+
+
+public class Ejercicios
+{	
+	//devuelve true si numero es multiplo de 3, de lo contrario devuelve false
+	//2 puntos
+	static boolean esMultiploDeTres(int num)
+	{
+		
+	}
+		return false;
+	}
+	
+	//devuel
+
+	
+	
+	
+	
+	//devuelve el promedio los elementos de un arreglo
+	//5 puntos
+	int getPromedio(int arreglo[])
+	
+	
+	 
+	private void scanf(String string, char[] apellido) {
+		// TODO Auto-generated method stub
+		
+	}
+	private void printf(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	//Realizar una funcion con las siguientes indicaciones:
+	//Si el parametro recibido es "Honduras" devolvera "Tegucigalpa"
+	//Si el parametro recibido es "Francia" devolvera "Paris"
+	//Si el parametro recibido es "Inglaterra" devolvera "Londres"
+	static String getCapital(String pais)
+	{
+		return "";
+	}
+	
+	//realizarla siguiente funcion recurisva:
+	
+	/* Pseudocodigo*/
+	//funcionRecursiva(num)
+	//  si num == 0
+	//    devolver 0
+	//  fin si
+	//  devolver num + funcionRecursiva(num-1)
+	
+	/*Notacion matematica*/
+	//funcionRecusriva(num) = num + funcionRecursiva(num-1)
+	//Donde funcionRecursiva(0) = 0
+	
+	//1 punto oro extra
+	static int funcionRecursiva(int num)
+	{
+		return -1;
+	}
+	
+	public static void main(String args[])
+	
